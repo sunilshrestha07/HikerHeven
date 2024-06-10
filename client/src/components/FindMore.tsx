7699baa6de4c425e2454d1fa9b0d509914f73d0d
Your Name <sunilcrestha9@gmai.com>
2024-06-10 20:25:35 +0545

    made some changes

--- client/src/components/FindMore.tsx
+++ client/src/components/FindMore.tsx
@@ -1,34 +1,24 @@
-import { useState } from "react";
+
 import { Link } from "react-router-dom";
 
 export default function FindMore() {
-    const [isHighImageLoaded, setIsHighImageLoaded] = useState<boolean>(false);
 
     return (
         <div className="">
             <div className="sm:mt-8">
                 <div className="grid grid-cols-1 sm:grid-cols-2 rounded-xl overflow-hidden">
                     <div className="row-span-1 sm:col-span-1">
-                        {isHighImageLoaded ? (
-                            <img 
-                                className="w-full aspect-auto object-cover object-center" 
-                                src="/crouselImage/findHigh.jpg" 
-                                alt="High quality nature trail" 
-                            />
-                        ) : (
-                            <img 
-                                className="w-full aspect-auto object-cover object-center" 
-                                src="/crouselImage/find.jpg" 
-                                alt="Nature trail" 
-                                onLoad={() => setIsHighImageLoaded(true)}
-                            />
-                        )}
+                        <img 
+                            className="w-full aspect-auto object-cover object-center" 
+                            src="/crouselImage/find.jpg" 
+                            alt="Nature trail" 
+                        />
                     </div>
                     <div className="row-span-1 sm:col-span-1 bg-darkGreen w-full h-full px-6 py-3 lg:px-16 lg:py-5 flex flex-col gap-4 sm:gap-6 justify-center items-start">
-                        <div className="font-Lora text-2xl sm:text-xl lg:text-3xl font-bold text-white">
+                        <div className="font-Lora text-2xl sm:text-xl lg:text-3xl font-semibold text-white">
                             <p>Step into nature's embrace, one trail at a time</p>
                         </div>
-                        <p className="font-Manrope text-sm sm:text-xs lg:text-base text-white">
+                        <p className="font-Quicksand text-sm sm:text-xs lg:text-base text-white">
                             Embark on a journey of discovery as you lace up your boots and venture into the great outdoors.
                         </p>
                         <Link to="/level/all">
