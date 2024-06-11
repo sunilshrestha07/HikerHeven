986004a4add688eeb64168b35a6bb2d014091da4
Your Name <sunilcrestha9@gmai.com>
2024-06-11 13:10:45 +0545

    made contact us page

--- client/src/pages/ContactUs.tsx
+++ client/src/pages/ContactUs.tsx
@@ -1,5 +1,58 @@
+import React, { useState } from "react";
+import { paddingSize } from "../declareSize";
+
+interface messageState{
+  email?:string,
+  message?:string
+}
 export default function ContactUs() {
+  const [message,setMessage] = useState<messageState>({})
+    const handelMessage = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    setMessage({...message,[e.target.id]:e.target.value})
+  }
+  //handeling mail submittion
+  const handelSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Form submitted',message)
+  };
+
+  
   return (
-    <div>ContactUs</div>
+    <>
+      <div className={`${paddingSize}`}>
+        <div className=" flex flex-col justify-center items-center bg-gray-200 rounded-lg gap-10 pt-20 pb-28 mt-10">
+          <div className=" font-Lora flex flex-col gap-3 justify-center items-center">
+            <p className=" text-2xl sm:text-4xl font-semibold">Contact Us</p>
+            <p className=" text-sm sm:text-xl font-semibold">Have questions or feedback? Reach out to us!</p>
+          </div>
+          <div className="sm:w-1/2">
+            <form className="w-full flex flex-col gap-4 font-Quicksand" onSubmit={handelSubmit}>
+
+                <div className=" flex flex-col ">
+                  <label  className=" text-sm sm:text-base font-semibold">Email</label>
+                  <input 
+                  className=" py-2 rounded-md px-4"
+                  type="email" name="" id="email"
+                  placeholder="Enter your email" 
+                  onChange={handelMessage} />
+                </div>
+
+                <div className=" flex flex-col">
+                  <label  className=" text-sm sm:text-base font-semibold">Message</label>
+                  <input 
+                  className=" py-2 rounded-md px-4" 
+                  type="text" name="" id="message" 
+                  placeholder="Message" 
+                  onChange={handelMessage} />
+                </div>
+
+                <div className=" flex justify-center mt-10">
+                  <button className=" px-6 py-3 font-Lora font-semibold w-1/2 rounded-2xl bg-darkGreen text-white">Submit</button>
+                </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
