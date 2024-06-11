c5c2b4467f3a492addfc8e66ac4895d8ee6e8d35
Your Name <sunilcrestha9@gmai.com>
2024-06-11 21:53:30 +0545

    made hike preview page

--- client/src/pages/AboutHike.tsx
+++ client/src/pages/AboutHike.tsx
@@ -1,5 +1,38 @@
+import { paddingSize } from "../declareSize"
+
 export default function AboutHike() {
+  const aboutHike = {name:'ChampaDevi',location:'Laltipur',rating:'4.5',image:'/crouselImage/lowOne.jpg',level:'hard'}
+
   return (
-    <div>AboutHike</div>
+    <>
+      <div className="">
+        <div className={`${paddingSize} mt-10`}>
+          <div className=" rounded-lg overflow-hidden ">
+              <div className=" relative w-full h-full">
+                <div className="">
+                  <img className=" w-full aspect-video sm:aspect-[5/2] object-cover object-center" src={aboutHike.image} alt="" />
+                </div>
+                {/* discrition about the hike */}
+                <div className="font-Lora absolute bottom-0 left-0 text-white w-full h-full flex flex-col justify-end items-start py-5 px-10 bg-gradient-to-t from-black to-transparent gap-2"></div>
+
+                <div className="font-Lora text-white flex flex-col justify-end items-start py-2 px-4 sm:gap-2 absolute bottom-2 sm:bottom-5 sm:left-5">
+                    <div className=" flex flex-row gap-2 sm:gap-4 items-center">
+                      <p className=" text-xl sm:text-5xl">{aboutHike.name}</p>
+                        <div className=" bg-white p-1 sm:p-2 flex flex-col justify-center items-center rounded-full aspect-square">
+                          <img className=" h-3 sm:h-4" src="/navImages/save.png" alt="" />
+                        </div>
+                    </div>
+                    <div className=" opacity-95 flex flex-row items-center gap-2 ">
+                      <p className="text-base sm:text-2xl">{aboutHike.level}</p>
+                        <img className=" h-3 sm:h-5" src="/navImages/ystar.png" alt="" />
+                          <p className="text-base sm:text-2xl">{aboutHike.rating}</p>
+                    </div>
+                    <p className="text-base sm:text-2xl">{aboutHike.location}</p>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
--- client/src/pages/HikesPage.tsx
+++ client/src/pages/HikesPage.tsx
@@ -19,39 +19,39 @@ export default function HikesPage() {
   return (
     <>
       <div className="">
-        <div className={paddingSize}>
+        <div className={`${paddingSize} flex flex-col gap-10`}>
           <div className="">
-            <p className="font-Lora text-4xl font-semibold">Find your best hike</p>
+            <p className="font-Lora text-xl sm:text-4xl font-semibold">Find your best hike</p>
           </div>
           <div className="">
-          <div className=" grid grid-cols-2 sm:grid-cols-4 gap-8 sm:gap-8  ">
-                            {favorite
-                            //showing only 4 hikes
-                            .map((hike,index)=>(
-                                <div className=" col-span-1 cursor-pointer relative" key={index}>
-                                    <Link to='/aboutHike'>
-                                        <div className="">
-                                            <div className="w-full aspect-video object-cover rounded-xl overflow-hidden">
-                                                <img className=" w-full h-full hover:scale-110 transition ease-in-out duration-200" src={hike.image} alt="" />
-                                            </div>
-                                            <div className=" mt-1 p-1">
-                                                <p className="text-xs sm:text-base font-Lora font-semibold">{hike.name}</p>
-                                                <p className="text-xs sm:text-base font-Lora opacity-90">{hike.location}</p>
-                                                <div className=" flex gap-2  items-center">
-                                                    <p className="text-xs sm:text-base  font-Lora opacity-90">{hike.rating}</p>
-                                                    <img src="/navImages/star.png" className="h-3 " alt="" />
-                                                    <p className="text-xs sm:text-base  font-Lora opacity-90">{hike.level}</p>
-                                                </div>
-                                            </div>
-                                        </div>  
-                                    </Link>
-                                    {/* for adding to saved */}
-                                    <div className="flex justify-center items-center bg-white rounded-full absolute top-2 right-2 p-1">
-                                        <img className=" h-3 sm:h-5" src="/navImages/save.png" alt="" />
+            <div className=" grid grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-8  ">
+                {favorite
+                //showing only 4 hikes
+                .map((hike,index)=>(
+                    <div className=" col-span-1 cursor-pointer relative" key={index}>
+                        <Link to='/aboutHike'>
+                            <div className="">
+                                <div className="w-full aspect-video object-cover rounded-xl overflow-hidden">
+                                    <img className=" w-full h-full hover:scale-110 transition ease-in-out duration-200" src={hike.image} alt="" />
+                                </div>
+                                <div className=" mt-1 p-1">
+                                    <p className="text-xs sm:text-base font-Lora font-semibold">{hike.name}</p>
+                                    <p className="text-xs sm:text-base font-Lora opacity-90">{hike.location}</p>
+                                    <div className=" flex gap-2  items-center">
+                                        <p className="text-xs sm:text-base  font-Lora opacity-90">{hike.rating}</p>
+                                        <img src="/navImages/star.png" className="h-3 " alt="" />
+                                        <p className="text-xs sm:text-base  font-Lora opacity-90">{hike.level}</p>
                                     </div>
                                 </div>
-                            ))}
+                            </div>  
+                        </Link>
+                        {/* for adding to saved */}
+                        <div className="flex justify-center items-center bg-white rounded-full absolute top-2 right-2 p-1">
+                            <img className=" h-3 sm:h-5" src="/navImages/save.png" alt="" />
                         </div>
+                    </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
